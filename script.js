ed38d31bef131dad8ca1f0e38818bfc336f315aa
Adib_Bh <121905691+Adib-96@users.noreply.github.com>
2025-08-09 20:16:56 +0100

    Add user image and update index.html and script.js for improved styling and functionality

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const sideMenu = document.getElementById('sideMenu');
-
+const navbar = document.querySelector('nav');
+const navLinks = document.querySelector('nav ul');
 
 function openMenu() {
     sideMenu.style.transform = 'translateX(-16rem)';
@@ -7,3 +8,13 @@ function openMenu() {
 function closeMenu() {
     sideMenu.style.transform = 'translateX(16rem)';
 }
+
+window.addEventListener('scroll', () => {
+    if(scrollY > 50) {
+        navbar.classList.add('bg-white',"bg-opacity-50","backdrop-blur-lg","shadow-sm");
+        navLinks.classList.remove('bg-white','shadow-sm','bg-opacity-50');
+    }else {
+        navbar.classList.remove('bg-white',"bg-opacity-50","backdrop-blur-lg","shadow-sm");
+         navLinks.classList.add('bg-white','shadow-sm','bg-opacity-50');
+    }
+});
